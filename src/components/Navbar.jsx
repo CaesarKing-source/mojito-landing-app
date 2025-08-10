9ac62b851f8b4ffff18dac384b0cd237af342332
CaesarKing-source <coder2880@gmail.com>
2025-08-11 02:43:48 +0530

    'navbar'

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { navLinks } from '../contants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const Navbar = () => {
+  useGSAP(() => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: 'nav',
+            start: 'bottom top',
+        }
+    });
+
+    tl.fromTo('nav', { background: 'transparent'}, {
+        background: '#00000050',
+        backgroundFilter: 'blur(10px)',
+        duration: 0.5,
+        ease: 'power1.inOut'
+    })
+  }, [])
+  return (
+    <nav>
+      <div>
+        <a href="#home" className='flex items-center gap-2'>
+            <img src="/images/logo.png" alt="logo" />
+            <p>Velvet Pour</p>
+        </a>
+        <ul>
+            { navLinks.map((link, index) => {
+                return <li key={index}>
+                    <a href={link.id}>{link.title}</a>
+                </li>
+            })}
+        </ul>
+      </div>    
+    </nav>
+  )
+}
+
+export default Navbar
